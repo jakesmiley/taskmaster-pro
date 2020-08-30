8b1481c6c12fb2c01695de805866ad54cdf1e564
Jake Smiley <jake.alan.smiley@gmail.com>
2020-08-30 12:41:53 -0600

    final touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,7 +184,7 @@ $("#modalDueDate").datepicker({
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -221,22 +221,22 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event) {
     $(this).addClass("dropover")
-    $(".bottom-trash", this).addClass("bottom-trash-drag")
+    $(".bottom-trash").addClass("bottom-trash-drag")
     console.log("activate", this);
   },
   deactivate: function(event) {
     $(this).removeClass("dropover")
-    $(".bottom-trash", this).removeClass("bottom-trash-drag")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
     console.log("deactivate", this);
   },
   over: function(event) {
     $(this).addClass("dropover-active")
-    $(".bottom-trash", this).addClass("bottom-trash-active")
+    $(".bottom-trash").addClass("bottom-trash-active")
     console.log("over", event.target);
   },
   out: function(event) {
     $(this).removeClass("dropover-active")
-    $(".bottom-trash", this).removeClass("bottom-trash-active")
+    $(".bottom-trash").removeClass("bottom-trash-active")
     console.log("out", event.target);
   },
   update: function(event) {
